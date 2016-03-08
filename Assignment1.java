829b103d915e5dfd45d370aff1d8a549af5fb43b
Liuri Loami <liuriloami@gmail.com>
2016-03-08 19:14:25 +0000

    Assignment 1 is now functional, but it needs reviewed.

--- Assignment1.java
+++ Assignment1.java
@@ -1,8 +1,14 @@
 import lejos.nxt.*;
-
+import lejos.robotics.navigation.*;
+import java.lang.Math;
 public class Assignment1 {
     public static void main (String[] args) {
-        // State 1: Print "Assignment 1" on the screen
+        SoundSensor soundSensor = new SoundSensor(SensorPort.S1);
+        DifferentialPilot pilot = new DifferentialPilot(2.1f, 12f, Motor.A, Motor.B, true);
+		UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S2);
+        LightSensor lightSensor = new LightSensor(SensorPort.S4);
+		
+		// State 1: Print "Assignment 1" on the screen
         System.out.println("Assignment 1");
 
         // State 2: Wait for a button to be pressed
@@ -11,28 +17,29 @@ public class Assignment1 {
         // State 3: Clear the screen
         LCD.clear();
 
-        // TODO!
         // State 4: Wait for clapping and print "Claps" on the screen when it happens
-        SoundSensor soundSensor = new SoundSensor(SensorPort.S1);
-        while (!isClap()) {
-            int soundLevel = soundSensor.readValue();
-            processSound();
-        }
+        int soundLevel = soundSensor.readValue();
+		int maxlvl = 0;
+		while (soundLevel < 70) {
+			maxlvl = Math.max(maxlvl, soundLevel);
+			soundLevel = soundSensor.readValue();
+		}
+		System.out.println("Claps");
 
-        // TODO!
-        // State 5: Move forward until detecting a hard surface underneath
-
-        // State 6: Turn 90 degrees to the left
-        pilot.rotate(-180);
+        // State 5: . Move forward until detecting a light surface underneath
+        pilot.setTravelSpeed(5);
+		while (lightSensor.readNormalizedValue() < 300)
+            pilot.backward();
+        
+		// State 6: Turn 90 degrees to the left
+		pilot.rotate(-90);
         
         // State 7: Move forward until a hard surface is detected by sonar at less than 25 cm distante
-        UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S2);
-        DifferentialPilot pilot = new DifferentialPilor(2.1f, 2.1f, Motor.A, Motor.B, true);
-        pilot.setRobotSpeed(15);
+        pilot.setTravelSpeed(5);
 
         while (ultrasonicSensor.getDistance() > 25)
-            pilot.forward();
-
+			pilot.backward();
+            
         // State 8: Stop and turn 180 degrees
         // TODO: Is it really necessary to stop manually?
         pilot.stop();
@@ -40,7 +47,7 @@ public class Assignment1 {
 
         // State 9: Move forward 20 units, then stop
         // TODO: Is it really necessary to stop manually?
-        pilot.travel(20);
+        pilot.travel(-20);
         pilot.stop();
 
         // State 10: Turn 90 degrees to the right
@@ -48,8 +55,8 @@ public class Assignment1 {
 
         // State 11: Move forward until the touch sensor causes the robot to stop.
         TouchSensor touchSensor = new TouchSensor(SensorPort.S3);
-        while (!isPressed())
-            pilot.forward();
+        while (!touchSensor.isPressed())
+            pilot.backward();
 
         // State 12: Print "Stop" on the screen.
         System.out.println("Stop");
