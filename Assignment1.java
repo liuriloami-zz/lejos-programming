a6e7d0a852be1fe6d0d58195f4f4339e4ff83efa
Liuri Loami <liuriloami@gmail.com>
2016-03-15 12:57:56 +0000

    Behaviour programming

--- Assignment1.java
+++ Assignment1.java
@@ -1,64 +1,165 @@
 import lejos.nxt.*;
 import lejos.robotics.navigation.*;
 import java.lang.Math;
+import lejos.robotics.subsumption.*;
+
 public class Assignment1 {
+
+    public class PrintAssignment  implements Behavior {
+       private boolean suppressed = false;
+       
+       public boolean takeControl() {
+          return true;
+       }
+
+       public void suppress() {
+          suppressed = true;
+       }
+
+       public void action() {
+         suppressed = false;
+         System.out.println("Assignment 1");
+         while( !suppressed )
+            Thread.yield();
+       }
+    }
+    
+    public class ClearScreen  implements Behavior {
+       private boolean suppressed = false;
+       
+       public boolean takeControl() {
+          return Button.readButtons() > 0;
+       }
+
+       public void suppress() {
+          suppressed = true;
+       }
+
+       public void action() {
+         suppressed = false;
+         LCD.clear();
+         while( !suppressed )
+            Thread.yield();
+       }
+    }
+    
+    public class WaitClaps  implements Behavior {
+        private boolean suppressed = false;
+        private SoundSensor soundSensor;
+        
+        public WaitClaps(SoundSensor soundSensor) {
+            this.soundSensor = soundSensor;
+        }
+        
+        public boolean takeControl() {
+            return this.soundSensor.readValue() < 70;
+        }
+
+        public void suppress() {
+            suppressed = true;
+        }
+
+        public void action() {
+            suppressed = false;
+            System.out.println("Claps");
+            pilot.forward();
+            while( !suppressed )
+                Thread.yield();
+        }
+    }
+    
+    public class LightSurfaceUnderneath  implements Behavior {
+        private boolean suppressed = false;
+        private LightSensor lightSensor;
+        
+        public WaitClaps(LightSensor lightSensor) {
+            this.lightSensor = lightSensor;
+        }
+        
+        public boolean takeControl() {
+            return this.lightSensor.readNormalizedValue() < 300;
+        }
+
+        public void suppress() {
+            suppressed = true;
+        }
+
+        public void action() {
+            suppressed = false;
+            pilot.rotate(-90);
+            pilot.forward();
+            while( !suppressed )
+                Thread.yield();
+        }
+    }
+    
+    public class SonarHardSurface  implements Behavior {
+        private boolean suppressed = false;
+        private UltrasonicSensor ultrasonicSensor;
+        
+        public WaitClaps(UltrasonicSensor ultrasonicSensor) {
+            this.ultrasonicSensor = ultrasonicSensor;
+        }
+        
+        public boolean takeControl() {
+            return this.ultrasonicSensor.getDistance() > 25;
+        }
+
+        public void suppress() {
+            suppressed = true;
+        }
+
+        public void action() {
+            suppressed = false;
+            pilot.rotate(180);
+            pilot.travel(-20);
+            pilot.rotate(90);
+            pilot.forward();
+            while( !suppressed )
+                Thread.yield();
+        }
+    }
+    
+    public class SonarHardSurface  implements Behavior {
+        private boolean suppressed = false;
+        private TouchSensor touchSensor;
+        
+        public WaitClaps(TouchSensor touchSensor) {
+            this.touchSensor = touchSensor;
+        }
+        
+        public boolean takeControl() {
+            return this.touchSensor.isPressed();
+        }
+
+        public void suppress() {
+            suppressed = true;
+        }
+
+        public void action() {
+            suppressed = false;
+            System.out.println("Stop");
+        }
+    }
+    
     public static void main (String[] args) {
+        //Sensors
         SoundSensor soundSensor = new SoundSensor(SensorPort.S1);
         DifferentialPilot pilot = new DifferentialPilot(2.1f, 12f, Motor.A, Motor.B, true);
 		UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S2);
         LightSensor lightSensor = new LightSensor(SensorPort.S4);
 		
-		// State 1: Print "Assignment 1" on the screen
-        System.out.println("Assignment 1");
-
-        // State 2: Wait for a button to be pressed
-        Button.waitForAnyPress();
-
-        // State 3: Clear the screen
-        LCD.clear();
-
-        // State 4: Wait for clapping and print "Claps" on the screen when it happens
-        int soundLevel = soundSensor.readValue();
-		int maxlvl = 0;
-		while (soundLevel < 70) {
-			maxlvl = Math.max(maxlvl, soundLevel);
-			soundLevel = soundSensor.readValue();
-		}
-		System.out.println("Claps");
-
-        // State 5: . Move forward until detecting a light surface underneath
-        pilot.setTravelSpeed(5);
-		while (lightSensor.readNormalizedValue() < 300)
-            pilot.backward();
-        
-		// State 6: Turn 90 degrees to the left
-		pilot.rotate(-90);
-        
-        // State 7: Move forward until a hard surface is detected by sonar at less than 25 cm distante
-        pilot.setTravelSpeed(5);
-
-        while (ultrasonicSensor.getDistance() > 25)
-			pilot.backward();
-            
-        // State 8: Stop and turn 180 degrees
-        // TODO: Is it really necessary to stop manually?
-        pilot.stop();
-        pilot.rotate(180);
-
-        // State 9: Move forward 20 units, then stop
-        // TODO: Is it really necessary to stop manually?
-        pilot.travel(-20);
-        pilot.stop();
-
-        // State 10: Turn 90 degrees to the right
-        pilot.rotate(90);
-
-        // State 11: Move forward until the touch sensor causes the robot to stop.
-        TouchSensor touchSensor = new TouchSensor(SensorPort.S3);
-        while (!touchSensor.isPressed())
-            pilot.backward();
-
-        // State 12: Print "Stop" on the screen.
-        System.out.println("Stop");
+		pilot.setTravelSpeed(5);
+		
+		Behaviour[] behaviours = {};
+		behaviours[0] = new PrintAssignment();
+		behaviours[1] = new ClearScreen();
+		behaviours[2] = new WaitClaps(soundSensor);
+		behaviours[3] = new LightSurfaceUnderneath(lightSensor);
+		behaviours[4] = new SonarHardSurface(ultrasonicSensor);
+		behaviours[6] = new TouchStop(touchSensor);
+	
+		Arbritrator arby = new Arbritrator(behaviours, true);
+		arby.start();
     }
 }
